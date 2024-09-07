5acfa073086be3cd1247baa6ac93458b9e9395b1
NesaValentina <tentanglangit914@gmail.com>
2024-09-07 20:05:13 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#element", {
+  strings: [
+    "Vectorist", 
+    "Design Illustrator",  
+    "Graphic Designer",
+    "Freelancer" 
+  ],
+  typeSpeed: 100,
+  loop: true,
+  shuffle: true
+})
